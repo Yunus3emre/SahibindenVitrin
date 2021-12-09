7f30f60bd06918ef6314a4fa02de3735c17c3cbb
Yunus3emre <ayanyunusemre3@gmail.com>
2021-12-09 12:12:54 +0300

    Add project files.

---
+++ SahibindenVitrin/Program.cs
@@ -0,0 +1,49 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Linq;
+
+namespace SahibindenVitrin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            Sahibinden();            
+        }
+
+        private static void Sahibinden()
+        {
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument document = web.Load("https://www.sahibinden.com/");
+            
+            String detayLink="";            
+            String sahibindenLink = "https://www.sahibinden.com";
+
+            for (int j = 1; j < 56; j++)
+            {                
+                var link = document.DocumentNode.SelectNodes("//*[@id=\"container\"]/div[3]/div/div[3]/div[3]/ul/li["+j+"]/a");
+                int last = 0;
+                link.ToList().ForEach(i => last = (i.OuterHtml.IndexOf(" title=")));
+                link.ToList().ForEach(i => detayLink = (i.OuterHtml.Substring(9, last - 10)));
+                Console.WriteLine(j+". ilan");
+                Details(sahibindenLink + detayLink);
+            }
+                    
+
+        }
+        private static void Details(String url)
+        {
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument document = web.Load(url);
+
+            var title = document.DocumentNode.SelectNodes("//*[@id=\"classifiedDetail\"]/div/div[1]/h1/text()").First().InnerText;
+            var price = document.DocumentNode.SelectNodes("//*[@id=\"classifiedDetail\"]/div/div[2]/div[2]/h3/text()").First().InnerText;
+            var link = url;
+
+            Console.WriteLine(title);
+            Console.WriteLine(price.Trim()) ;
+            Console.WriteLine(url);
+            Console.WriteLine("");
+        }
+    }
+}
