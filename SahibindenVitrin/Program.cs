977e8341d82c32e61a2b8e8ad3771d75cc83fb41
Yunus3emre <ayanyunusemre3@gmail.com>
2021-12-09 16:42:40 +0300

    update

--- SahibindenVitrin/Program.cs
+++ SahibindenVitrin/Program.cs
@@ -19,12 +19,13 @@ namespace SahibindenVitrin
             String detayLink="";            
             String sahibindenLink = "https://www.sahibinden.com";
 
-            for (int j = 1; j < 56; j++)
-            {                
+            for (int j = 1; j < 57; j++)
+            {
+                System.Threading.Thread.Sleep(30000);
                 var link = document.DocumentNode.SelectNodes("//*[@id=\"container\"]/div[3]/div/div[3]/div[3]/ul/li["+j+"]/a");
                 int last = 0;
                 Console.WriteLine("");                
-                link.ToList().ForEach(i => Console.WriteLine(i.OuterHtml));
+                //link.ToList().ForEach(i => Console.WriteLine(i.OuterHtml));
                 Console.WriteLine("");
                 link.ToList().ForEach(i => last = (i.OuterHtml.IndexOf(" title=")));
 
@@ -47,27 +48,26 @@ namespace SahibindenVitrin
             {
                 HtmlWeb web = new HtmlWeb();
                 HtmlDocument document = web.Load(url);
-                System.Threading.Thread.Sleep(3000);
+                
 
-                var title = document.DocumentNode.SelectNodes("//*[@id=\"classifiedDetail\"]/div/div[1]/h1/text()").First().InnerText;
-                System.Threading.Thread.Sleep(3000);
+                var title = document.DocumentNode.SelectNodes("//*[@id=\"classifiedDetail\"]/div/div[1]/h1/text()").First().InnerText;                
                 var price = document.DocumentNode.SelectNodes("//*[@id=\"classifiedDetail\"]/div/div[2]/div[2]/h3/text()").First().InnerText;
-                System.Threading.Thread.Sleep(3000);
+                
 
                 Console.WriteLine("");
                 Console.WriteLine(title);
                 Console.WriteLine(price.Trim());
                 Console.WriteLine(url);
-                Console.WriteLine("");
-                Console.WriteLine("");
+                Console.WriteLine("");                
             }
             catch (Exception)
             {
                 Console.WriteLine("");
                 Console.WriteLine("");
-                Console.WriteLine("Bu Bir Reklamdır");
+                Console.WriteLine("!!!Bir Hata Oluştu!!!");
                 Console.WriteLine("");
                 Console.WriteLine("");
+                System.Threading.Thread.Sleep(10000);
             }
 
 
