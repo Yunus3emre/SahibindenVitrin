e460ce1c04bb9bfc920cb5bf8c4d1a3b0c65c696
Yunus3emre <ayanyunusemre3@gmail.com>
2021-12-09 15:14:05 +0300

    update 2

--- SahibindenVitrin/Program.cs
+++ SahibindenVitrin/Program.cs
@@ -23,26 +23,55 @@ namespace SahibindenVitrin
             {                
                 var link = document.DocumentNode.SelectNodes("//*[@id=\"container\"]/div[3]/div/div[3]/div[3]/ul/li["+j+"]/a");
                 int last = 0;
+                Console.WriteLine("");                
+                link.ToList().ForEach(i => Console.WriteLine(i.OuterHtml));
+                Console.WriteLine("");
                 link.ToList().ForEach(i => last = (i.OuterHtml.IndexOf(" title=")));
-                link.ToList().ForEach(i => detayLink = (i.OuterHtml.Substring(9, last - 10)));
-                Console.WriteLine(j+". ilan");
-                Details(sahibindenLink + detayLink);
-                System.Threading.Thread.Sleep(10000);
+
+                if (last!=-1)
+                {
+                    link.ToList().ForEach(i => detayLink = (i.OuterHtml.Substring(9, last - 10)));
+                    Console.WriteLine(j + ". ilan");
+                    Details(sahibindenLink + detayLink);
+                }
+                else
+                {
+                    continue;
+                }               
+                
             }      
         }
         private static void Details(String url)
-        {   
-            HtmlWeb web = new HtmlWeb();
-            HtmlDocument document = web.Load(url);
+        {
+            try
+            {
+                HtmlWeb web = new HtmlWeb();
+                HtmlDocument document = web.Load(url);
+                System.Threading.Thread.Sleep(3000);
 
-            var title = document.DocumentNode.SelectNodes("//*[@id=\"classifiedDetail\"]/div/div[1]/h1/text()").First().InnerText;
-            var price = document.DocumentNode.SelectNodes("//*[@id=\"classifiedDetail\"]/div/div[2]/div[2]/h3/text()").First().InnerText;
-            var link = url;
+                var title = document.DocumentNode.SelectNodes("//*[@id=\"classifiedDetail\"]/div/div[1]/h1/text()").First().InnerText;
+                System.Threading.Thread.Sleep(3000);
+                var price = document.DocumentNode.SelectNodes("//*[@id=\"classifiedDetail\"]/div/div[2]/div[2]/h3/text()").First().InnerText;
+                System.Threading.Thread.Sleep(3000);
 
-            Console.WriteLine(title);
-            Console.WriteLine(price.Trim());
-            Console.WriteLine(url);
-            Console.WriteLine(""); 
+                Console.WriteLine("");
+                Console.WriteLine(title);
+                Console.WriteLine(price.Trim());
+                Console.WriteLine(url);
+                Console.WriteLine("");
+                Console.WriteLine("");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("Bu Bir Reklamdır");
+                Console.WriteLine("");
+                Console.WriteLine("");
+            }
+
+
+            
         }
     }
 }
